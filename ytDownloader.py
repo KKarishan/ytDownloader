331e52910950ca36377064dbd1eaeaf0d65f8d96
Keshon Karishan <keshon.k@affiniti.biz>
2023-11-13 08:41:01 +0000

    second commit

--- ytDownloader.py
+++ ytDownloader.py
@@ -20,3 +20,8 @@ audio_stream = yt.streams.filter(only_audio = True, file_extension='mp4').first(
 audio_stream.download('C:\\Users\\ABCD\\Desktop\\Music', filename = f"{yt.title}.mp3")
 
 print("Download Completed.")
+
+# type below line in your terminal 
+# python ytDownloader.py YouTubeLink
+# or
+# python3 ytDownloader.py YouTubeLink
